a0537c65ee44abe093535a57a23da4d73aaacc89
Brian Kemper <bkemper@gmail.com>
2016-03-02 16:49:04 -0700

    working

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var express = require('express'),
-  app = express(),
-  port = 8080,
-  bodyParser = require('body-parser'),
-  session = require('express-session'),
-  _ = require('lodash'),
-  config = require('./config.json');
+var express = require('express');
+var app = express();
+var port = 8080;
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var _ = require('lodash');
+var config = require('./config.js');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(session());
+app.use(session({secret: config.sessionSecret,resave: false, saveUninitialized: true}));
+
+console.log(config.sessionSecret);
 
 app.listen(port, function(){
   console.log('Listening on port ' + port);
